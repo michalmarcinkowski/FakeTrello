791b32a3f4a2426117ba89ea0815948d7f3a3ef4
michalmarcinkowski <michal.marcinkowski@lakion.com>
2014-10-20 23:07:54 +0200

    Added application bundle and initial templates

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('security/login.html.twig');
+    }
+}
