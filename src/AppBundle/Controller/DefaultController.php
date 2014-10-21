0c9e3789a49e4fa1ddf9effcc5172ba5112ad05b
michalmarcinkowski <michal.marcinkowski@lakion.com>
2014-10-21 11:15:38 +0200

    Login form template integration

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('security/login.html.twig');
+        return $this->render('default/index.html.twig');
     }
 }
