48fdbf4ab3ce1a4dfaf040d3ca6623a1eff33f06
lestreux <lestreux@gmail.com>
2014-12-16 11:37:34 +0100

    Organizations front-end handling

--- web/js/pages/board.js
+++ web/js/pages/board.js
@@ -1,7 +1,7 @@
- $( "body>div.container>div.boards" ).sortable().disableSelection();
+ $( "body>div.container>#section0>div.boards" ).sortable().disableSelection();
 
 
-$('#board-add-event').click(function(){
+$('.board-add-event').click(function(){
 	$(this).find('.pop-up').addClass('active');
 });
 $('#popup-close-event,a.btn.btn-danger.btn-lg').click(function(){
@@ -13,8 +13,9 @@ $('div.boards').on('click','.board-delete-event',function(){
 	return false;
 });
 
-$('#new-board-event').click(function(){
-	addBoard($('#app_board_name').val());
+$('.new-board-event').click(function(){
+	$section = $(this).parents('.boards-section');
+	addBoard($section.find('input.app_board_name').val(), $section.attr('id').replace(/\D/g,''));
 });
 
 
@@ -43,13 +44,14 @@ function deleteBoard($id){
 
 }
 
-function addBoard($name){
+function addBoard($name,$organization_id){
 	$ajax['data'] = {name:$name};
+	if($organization_id>0) $ajax['data']['organization'] = $organization_id;
 	$ajax['type'] = 'POST';
 	$ajax['url'] = 'boards/new/json';
 	$ajax['success'] = function(data){
-		$('.container>.boards').append('<a class="board" id="id'+data.id+'" href="/app_dev.php/boards/'+data.id+'"><h2 style="float: left;">'+data.name+'<span class="icon-star-empty star" data-href="/app_dev.php/boards/'+data.id+'/star"></span></h2><div class="pull-right"><button type="submit" class="btn btn-danger btn-confirm board-delete-event"><i class="icon-trash"></i> <span>Delete</span></button></div></a>');
-		$('#app_board_name').val('');
+		$('#section'+$organization_id+'>.boards').append('<a class="board" id="id'+data.id+'" href="/app_dev.php/boards/'+data.id+'"><h2 style="float: left;">'+data.name+'<span class="icon-star-empty star" data-href="/app_dev.php/boards/'+data.id+'/star"></span></h2><div class="pull-right"><button type="submit" class="btn btn-danger btn-confirm board-delete-event"><i class="icon-trash"></i> <span>Delete</span></button></div></a>');
+		//$('#app_board_name').val('');
 	};
 
 	$.ajax($ajax);
